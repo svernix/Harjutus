2e71afd86b49c415b0c8adcfa328061af693dc4c
Opilane TTHK <opilane@tthk.ee>
2026-01-21 10:00:53 +0200

    Lisasin Meetod1

---
+++ Meetodid/Meetod1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Meetod1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> filmid = GetMovies();
+            Console.WriteLine("Milline on sinu lemmikfilm");
+            string lemmikFilm = ReadAnswer();
+            DoesMovieExist(filmid, lemmikFilm);
+            filmid = DoYouLikeThisMovie(filmid, "Tron 1983");
+            //foreach (string item in filmid)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+        }
+
+        public static List<string> DoYouLikeThisMovie(List<string> collection, string movieToAdd)
+        {
+            if (collection.Contains(movieToAdd) == false)
+            {
+                Console.WriteLine($"Kas sulle meeldib{movieToAdd}?");
+                string vastus = ReadAnswer();
+
+                if(vastus == "jah")
+                {
+                    collection.Add(movieToAdd);
+                    Console.WriteLine("Lisasin filmi sulle");
+                }
+                else
+                {
+                    Console.WriteLine("Aga miks? See on ju hea film");
+                }
+
+            }
+            return collection;
+        }
+
+
+        public static void DoesMovieExist(List<string> collection, string findThis)
+        {
+            bool itExists = false;
+            foreach (string item in collection)
+            {
+                if (item == findThis)
+                {
+                    itExists = true;
+                }
+            }
+            if ( itExists == true)
+            {
+                Console.WriteLine("Näe mäletasid ikka!");
+            }
+            else
+            {
+                Console.WriteLine("a kus on sinu lemmikfilm");
+            }
+        }
+
+        public static string ReadAnswer()
+        {
+            string sisend = "";
+            while (sisend == "")
+            {
+                sisend = Console.ReadLine();
+            }
+            return sisend;
+        }
+
+
+        public static List<string> GetMovies()
+        {
+            Console.WriteLine("Mis on su lemmikfilmid, sisesta üks haaval, kui sisestada rohkem ei taha\n sisesta \"ei ole\"");
+            string sisestus = "";
+            List<string> siinOnFilmid = new List<string>();
+            while (sisestus != "ei ole")
+            {
+                Console.Write("Järgmine film:");
+                sisestus = Console.ReadLine();
+                if (sisestus != "ei ole")
+                {
+                    siinOnFilmid.Add(sisestus);
+                }
+            }
+            return siinOnFilmid;
+        }
+    }
+}
